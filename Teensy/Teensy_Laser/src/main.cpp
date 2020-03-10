ac1c08dcb8bb1c34196f1bcf5a648538bbbd8b00
nishi10000 <34505055+nishi10000@users.noreply.github.com>
2020-03-10 22:55:02 +0900

    余分なコメントの削除

--- Teensy/Teensy_Laser/src/main.cpp
+++ Teensy/Teensy_Laser/src/main.cpp
@@ -1,37 +1,19 @@
 /*
-  SD card read/write
+  SD card read
  
- This example shows how to read and write data to and from an SD card file 	
- The circuit:
  * SD card attached to SPI bus as follows:
  ** MOSI - pin 11, pin 7 on Teensy with audio board
  ** MISO - pin 12
  ** CLK - pin 13, pin 14 on Teensy with audio board
  ** CS - pin 4, pin 10 on Teensy with audio board
- 
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- 
- This example code is in the public domain.
- 	 
+  
  */
  
 #include <SD.h>
 #include <SPI.h>
 #include <MsTimer2.h>
 File myFile;
-// change this to match your SD shield or module;
-// Arduino Ethernet shield: pin 4
-// Adafruit SD shields and modules: pin 10
-// Sparkfun SD shield: pin 8
-// Teensy audio board: pin 10
-// Teensy 3.5 & 3.6 on-board: BUILTIN_SDCARD
-// Wiz820+SD board: pin 4
-// Teensy 2.0: pin 0
-// Teensy++ 2.0: pin 20
-const int chipSelect = BUILTIN_SDCARD;
+const int chipSelect = BUILTIN_SDCARD;// Teensy 3.5 & 3.6 on-board: BUILTIN_SDCARD
 #define file_name "img_test.txt"
 #define lazer_on_message "lazer_on,"
 #define lazer_off_message "lazer_off,"
@@ -41,7 +23,6 @@ const int chipSelect = BUILTIN_SDCARD;
 #define lazer_on 255
 #define lazer_off 0
 #define flame_time 30//30ms
-
 
 String str;
 String buff;
@@ -63,12 +44,10 @@ void flame_timer(){
 
 void setup()
 {
-  
   analogWriteResolution(12);
- // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+     // wait for serial port to connect. Needed for Leonardo only
   }
   Serial.print("Initializing SD card...");
   if (!SD.begin(chipSelect)) {
@@ -76,7 +55,6 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
-    // open the file. 
   MsTimer2::set(30, flame_timer); //30fps...15だったら60fps
   MsTimer2::start();
 }
@@ -85,46 +63,29 @@ void setup()
 void loop()
 {
   int flame_start_pos=0;
-	// nothing happens after setup
-   myFile = SD.open(file_name, FILE_WRITE);
-  // re-open the file for reading:
+	myFile = SD.open(file_name, FILE_WRITE);
   str="";//str初期化
   buff="";
-
   myFile = SD.open(file_name);
   if(!myFile){
     Serial.println("Can not open file!");
     return;
   }
-  
-  //if (myFile) {
-    //Serial.println(file_name);
-    //Serial.println("start_file_read");
-    // read from the file until there's nothing else in it:
     int count=0;
     while (myFile.available()) {
-      
       str = char(myFile.read());//1biteずつ格納される。
       count++;
-      //Serial.print("count=");
-      //Serial.println(count);
-      
       buff += str;
-      
       if(str==','){
         /*構想
         ：framestartの場所を記憶してflame時間分経過していなければ、ループさせる。
         */
-        
         if(buff.equals(frame_start_message)){
-          flame_start_pos=myFile.position();//2.28追記//
-          //Serial.println("FRAME_START");          
-          //buff="";
+          flame_start_pos=myFile.position();
         }else
         if(buff.equals(lazer_off_message)){
           Serial.println("LAZER_OFF");  
           analogWrite(lazer_output,lazer_off);        
-          //buff="";
         }else
         if(buff.equals(lazer_on_message)){
           Serial.println("LAZER_ON");
@@ -142,17 +103,11 @@ void loop()
           }else{
             flame_end=false;
           }
-          //Serial.println("FRAME_END");
-          //buff="";          
         }else{
-          //Serial.print("buff=");
           //Serial.println(buff);
           buff=buff.trim();
-          
           buff.remove(buff.indexOf(","), 1);
-          //Serial.println(buff);
           int val = buff.toInt();
-          //Serial.println(val);
           if(x_val_count==0){
             x_val_count++;
             x_val=val;
@@ -160,26 +115,14 @@ void loop()
             x_val_count=0;
             y_val=val;
           }
-          //Serial.print("x_val:");
-          //Serial.println(x_val);
-          //Serial.print("y_val:");
-          //Serial.println(y_val);
           analogWrite(A21, map(x_val,0,640,0,4095));
           analogWrite(A22, map(y_val,0,640,0,4095));
-          //Serial.print("map_y_val:");
-          Serial.println(map(y_val,0,640,0,4095));
+          //Serial.println(map(y_val,0,640,0,4095));
           while (wait_time < 3) ; // wait
           wait_time = wait_time - 3;
-          //buff="";
         }
         buff="";
       }
-      
     }
-    // close the file:
     myFile.close();
-  //} else {
-  	// if the file didn't open, print an error:
-    //Serial.println("error opening test.txt");
-  //}
 }
